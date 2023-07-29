1087f98a90463693e881384d4675b6acd4e4f88a
alexmarquez9 <140654902+alexmarquez9@users.noreply.github.com>
2023-07-29 11:40:07 -0400

    Update Demo1.java

--- src/day1/Demo1.java
+++ src/day1/Demo1.java
@@ -5,4 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Testing...");
 	}
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}	
 }
